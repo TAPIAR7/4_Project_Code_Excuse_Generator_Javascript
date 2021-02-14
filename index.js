211dc9fbf45c49d770b2702298ab63b65a79526f
TAPIAR7 <rtapiatec@gmail.com>
2021-02-14 02:24:09 +0000

    This the code excuse generator completed

---
+++ index.js
@@ -0,0 +1,17 @@
+
+let who = ['The dog','My grandma','His turtle','My bird'];
+let action = ['ate','peed','crushed','broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+
+let whoRandom = Math.floor(Math.random() * who.length);
+let actionRandom = Math.floor(Math.random() * action.length);
+let whatRandom = Math.floor(Math.random() * what.length);
+let whenRandom = Math.floor(Math.random() * when.length);
+
+let excuse = who[whoRandom] + " " + action[actionRandom] + " " + what[whatRandom] + " " + when[whenRandom] + ".";
+
+console.log(excuse);
+document.getElementById('excuse').innerText = excuse;
+let nodeH3 = document.getElementById('excuse');
+console.log(nodeH3);
